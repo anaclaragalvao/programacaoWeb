517d6e69ca14483bf9cf0fc845a8c93db011ff84
Ana Clara Galvão <71735795+anaclaragalvao@users.noreply.github.com>
2025-09-04 11:57:46 +0000

    agenda de contatos

---
+++ agendaContatos/exemplo/contatos/urls.py
@@ -0,0 +1,11 @@
+from django.urls.conf import path
+from contatos import views
+
+app_name = "contatos"
+
+urlpatterns = [
+path('lista/', views.ContatoListView.as_view(),
+ name='lista-contatos'),
+ path('', views.ContatoListView.as_view(),
+ name='home-contatos'),
+]
